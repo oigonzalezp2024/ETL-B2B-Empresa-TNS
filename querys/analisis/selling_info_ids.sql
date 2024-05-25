6b131dbd3970e71569be0d9181154736ff6d6914
Oscar Gonzalez <oigonzalezp2024@gmail.com>
2024-05-25 02:29:43 -0500

    Se agregan las querys básicas del desarrollo.

---
+++ querys/analisis/selling_info_ids.sql
@@ -0,0 +1,9 @@
+SELECT
+info_id,
+invoice_id,
+store_id,
+customer_id,
+ref_invoice_id,
+ref_user_id,
+pmethod_id
+FROM selling_info;
---
+++ querys/view_itemsdescuentos.sql
@@ -0,0 +1,12 @@
+SELECT 
+id_itemsdescuentos,
+codconcepto,
+valordto,
+baseretd,
+porcretd,
+porivad,
+centrocostos,
+codarea,
+venta_id
+FROM view_itemsdescuentos
+where venta_id = xxxx;
